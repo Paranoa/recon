6672017333db53c4a176f6f9b069000b4a778cf2
geex88 <chenyunwei@geexfinance.com>
2018-07-18 10:38:53 +0800

    解决合并冲突

--- vue.config.js
+++ vue.config.js
@@ -15,11 +15,6 @@ module.exports = {
   },
   devServer: {
     // proxy: 'https://beta.geexfinance.com/geex-merchant-recon/'
-<<<<<<< HEAD
     proxy: 'http://127.0.0.1:8081/geex-merchant-recon-provider/'
-=======
-    proxy: 'http://192.168.8.110:8081/geex-merchant-recon-provider/'
-    // proxy: 'http://192.168.8.59:8080/'
->>>>>>> 7ea27669ac51eb03bb817723e3cfe701b876534c
   }
 }
