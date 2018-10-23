168ca9ae3452a27fafb351d53862b98694b418a0
zhangwei <dazlzhang@163.com>
2018-10-23 14:18:14 +0800

    fix backdrop z-index

--- src/components/VBackDropMask.vue
+++ src/components/VBackDropMask.vue
@@ -2,5 +2,5 @@
   <div class="ui-back-mask"></div> 
 </template>
 <style>
-  .ui-back-mask { position: fixed; z-index: 10000; left: 0; top: 0; width: 100%; height: 100%; background: rgba(0,0,0,.3)}
+  .ui-back-mask { position: fixed; z-index: 11001; left: 0; top: 0; width: 100%; height: 100%; background: rgba(0,0,0,.3)}
 </style>
